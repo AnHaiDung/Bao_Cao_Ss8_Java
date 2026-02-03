ee060b4fd2935cfae1dfa26e288c8b7c3fbf010a
AnHaiDung <anhaidung2k6@gmail.com>
2026-02-03 09:46:15 +0700

    Delete HN_KS24_CNTT2_IT203_Session_08 directory

--- HN_KS24_CNTT2_IT203_Session_08/StudentManagementSystem/src/Message.java
+++
@@ -1,7 +0,0 @@
-public class Message {
-    public static final String DUPLICATE_ID = "ID không được trùng!";
-    public static final String EMPTY_DATA = "Dữ liệu không được để trống!";
-    public static final String INVALID_DATA = "Dữ liệu không hợp lệ!";
-    public static final String STUDENT_NOT_FOUND = "Không tìm thấy sinh viên này!";
-    public static final String STUDENTS_EMPTY = "Danh sách sinh viên trống!";
-}
